05ca9bc43236fb2618ecba630cfdd9869467e7d8
erezakri@gmail.com <0012Erez>
2016-08-12 12:50:18 +0300

    PDImageXObject

--- src/createPDF.java
+++ src/createPDF.java
@@ -3,9 +3,17 @@ import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.font.PDFont;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.pdmodel.graphics.PDXObject;
+import org.apache.pdfbox.pdmodel.graphics.image.*;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 
 //import java.awt.*;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Created by erez on 06/08/2016.
@@ -18,11 +26,16 @@ public class createPDF {
         doc.addPage(page);
         PDFont font = PDType1Font.HELVETICA_BOLD;
 
+//        InputStream in = new FileInputStream(new File("simley.png"));
+//        PDXObject img = new PDImageXObject(doc);
+
+        BufferedImage bufferedImage = ImageIO.read(new CalloutBorder());
         PDPageContentStream content = new PDPageContentStream(doc, page);
 
+        content.drawImage((PDImageXObject) img, 100, 700);
 
         content.close();
-        doc.save("PDFWithText.pdf");
+
         try {
             doc.save("Empty PDF.pdf");
             doc.close();
@@ -33,5 +46,10 @@ public class createPDF {
 
     }
 
+//    void addImageToPDF(PDDocument document , File file){
+//
+//        PDJpeg f = new PDJpeg;
+//
+//    }
 
 }
